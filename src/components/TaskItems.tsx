34c09ebcda8d217d58013e302ddaf7d340dd0953
AeshaHangRai <evilaesha@gmail.com>
2026-02-06 10:05:45 +0545

    Initial commit

---
+++ src/components/TaskItems.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from './ui/button';
+interface Props {
+    id : number;
+    title: string;
+    completed: boolean;
+    onDelete: (id: number) => void;
+    onToggle: (id: number) => void;
+}
+function TaskItems({ id, title, completed, onToggle, onDelete }: Props) {
+  return (
+    <div className="flex justify-between p-2 border-b">
+        <div className="flex items-center gap-2">
+            <input type="checkbox" checked={completed} onChange={() => onToggle(id)}/>
+        </div>
+      <span className={completed ? "line-through text-green-500" : ""}>
+        {title}
+        </span>
+      
+      <Button variant={'destructive'} onClick={() => onDelete(id)}>Delete</Button>
+    </div>
+  );
+}
+
+export default TaskItems;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,24 @@
+import type { Task } from '@/types/task';
+import React from 'react'
+import TaskItems from './TaskItems';
+interface Props {
+    tasks: Task[];
+    onToggle: (id: number) => void;
+    onDelete: (id: number) => void;
+}
+function TaskList({tasks, onToggle,onDelete}: Props) {
+  return(
+   <div>
+    {tasks.map((t)=> (
+    <TaskItems
+     id={t.id} 
+     title={t.title} 
+     completed={t.completed}
+      onToggle={onToggle} 
+      onDelete={onDelete} />
+  ))}
+  </div>
+  );
+}
+
+export default TaskList;
